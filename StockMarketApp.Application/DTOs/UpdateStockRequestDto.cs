c95ed5023adf46fbeddc9a21c5820bb8030ca4b4
Alexis Franco <alexfranco1575@gmail.com>
2026-02-08 09:20:35 -0500

    feat: Add data annotations for input validation

--- StockMarketApp.Application/DTOs/UpdateStockRequestDto.cs
+++ StockMarketApp.Application/DTOs/UpdateStockRequestDto.cs
@@ -1,23 +1,24 @@
+using System.CodeDom.Compiler;
 using System.ComponentModel.DataAnnotations;
 
 namespace StockMarketApp.Application.DTOs
 {
-    public class CreateStockRequest
+    public class UpdateStockRequestDto
     {
         [Required]
-        [MaxLength(10, ErrorMessage ="El símbolo no puede tener más de 10 caracteres.")]
+        [MaxLength(10, ErrorMessage = "El símbolo no puede tener más de 10 caracteres.")]
         public string Symbol {get; set; } = string.Empty;
 
         [Required]
         [MaxLength(100, ErrorMessage = "El nombre de acción no puede tener más de 100 caracteres.")]
-        public string CompanyName {get; set; } = string.Empty;
+        public string CompanyName {get; set; } =string.Empty;
 
         [Required]
         [Range(1, 1000000, ErrorMessage = "El precio debe estar entre 1 y 1.000.000")]
-        public decimal PurchasePrice {get; set; }
+        public decimal PurchasePrice {get; set;}
 
         [Required]
-        [Range(1, 100, ErrorMessage = "La cantidad debe estar entre 1 y 1000")]
-        public int Quantity {get; set; }
+        [Range(1, 1000, ErrorMessage = "La cantidad debe estar entre 1 y 1000")]
+        public int Quantity {get; set;}
     }
 }
